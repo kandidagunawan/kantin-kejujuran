41550d92b36499207a7e16273d18275019dd7ebf
kandidagunawan <65218153+kandidagunawan@users.noreply.github.com>
2022-07-10 19:56:51 +0700

    Update balance.php

--- balance.php
+++ balance.php
@@ -47,7 +47,7 @@ if(isset($_POST["submit"])){
         <h1>KANTIN KEJUJURAN</h1>
         <!-- <a href = login.php><img src = "login.jpg" width = 40px height =40px></a>  -->
         <ul>
-            <li> <a href = "login.php"> Home </a></li>
+            <li> <a href = "index.php"> Home </a></li>
             <li> <a href = "sell.php"> Start selling </a></li>
             <li> <a href = "balance.php"> Balance </a></li>
             <li> <a href="logout.php">Logout</a></li>
